c7a8679f4af0923fae24cedf0f3570a767b3a7b5
seankelly1994 <seankearsney@gmail.com>
2019-09-16 07:42:10 +0200

    Created the client page

--- react-front-end/src/components/createclient/index.js
+++ react-front-end/src/components/createclient/index.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-class CreateClient extends Component () {
+class CreateClient extends Component {
     constructor(props) {
         super(props);
 
@@ -12,6 +12,24 @@ class CreateClient extends Component () {
         const [businessPhone, setBusinessPhone] = useState('');
     }
     
-}
+    render() {
 
+        return (
+            <Form>
+                <Form.Group controlId="formFirstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter First Name"></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="formLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Last Name"></Form.Control>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+        )
+    }
+
+}
 export default CreateClient;
---
+++ react-front-end/src/containers/clientPage/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import CreateClient from '../../components/createclient/index';
+import Clients from '../../components/clients/index';
+
+class ClientPage extends Component {
+
+}
+
+export default ClientPage;
