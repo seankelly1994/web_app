4dac0c081bb11b2dd299a1e8e91f8c53ce7f37e1
seankelly1994 <seankearsney@gmail.com>
2019-09-16 20:05:58 +0200

    Added router

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Clients from './components/clients/index';
 import Sidebar from './components/sidebar/index';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   const [clients, setClients] = useState([]);
@@ -14,10 +15,14 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-    <Sidebar></Sidebar>
-      <Clients clients={clients}></Clients>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Sidebar></Sidebar>
+      <Route path="/"></Route>
+      <Route path="/clients" exact component={Clients}><Clients clients={clients}></Clients></Route>
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
