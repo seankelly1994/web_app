5591bd32727cde47f32c6060982aa9422ba7b257
seankelly1994 <seankearsney@gmail.com>
2019-10-02 07:15:18 +0200

    Updates

--- app/models.py
+++ app/models.py
@@ -7,7 +7,14 @@ class Client(db.Model):
     last_name = db.Column(db.String(50))
     email_address = db.Column(db.String(50))
     business_phone = db.Column(db.String(10))
-    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Client()>'.format(self.first_name)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email_address = db.Column(db.String(50))
+    clients = db.relationship('Client', backref='owner', lazy='dynamic')
--- react-front-end/src/components/createclient/index.js
+++ react-front-end/src/components/createclient/index.js
@@ -1,6 +1,8 @@
 import React, { Component, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import './style.css';
 
 const CreateClient = () => {
     
@@ -12,15 +14,36 @@ const CreateClient = () => {
 
         return (
             <Form>
-                <Form.Group controlId="formFirstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter First Name" value={firstName} onChange={e => setFirstName(e.target.value)}></Form.Control>
-                </Form.Group>
+                <Form.Row controlId="formName" className="RowDetails" className="justify-content-md-center">
+                    <Col sm={3}>
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter First Name" value={firstName} 
+                                        onChange={e => setFirstName(e.target.value)}>
+                        </Form.Control>
+                    </Col>
+                    <Col sm={3}>
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Last Name"  value={lastName} 
+                                        onChange={e => setLastName(e.target.value)}>
+                        </Form.Control>
+                    </Col>
+                </Form.Row>
 
-                <Form.Group controlId="formLastName">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Last Name"  value={lastName} onChange={e => setLastName(e.target.value)}></Form.Control>
-                </Form.Group>
+                <Form.Row controlId="formContactDetails" className="justify-content-md-center">
+                    <Col sm={3}>
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Email" value={emailAddress} 
+                                        onChange={e => setEmailAddress(e.target.value)}>
+                        </Form.Control>
+                    </Col>
+                    <Col sm={3}>
+                        <Form.Label>Business Phone</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Business Phone"  value={businessPhone} 
+                                        onChange={e => setBusinessPhone(e.target.value)}>
+                        </Form.Control>
+                    </Col>
+                </Form.Row>
+
 
                 <Button variant="primary" type="submit" onClick={async() => {
                     const client = {firstName, lastName};
